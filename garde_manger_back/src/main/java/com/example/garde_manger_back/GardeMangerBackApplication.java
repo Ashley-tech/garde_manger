e8318f9c8f6213569abc8797fbf3ec213299bc05
Ashley R. <ashleyrakotoarisoa@gmail.com>
2026-08-23 16:46:46 +0200

    Initializations projects back and front

---
+++ garde_manger_back/src/main/java/com/example/garde_manger_back/GardeMangerBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.garde_manger_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardeMangerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardeMangerBackApplication.class, args);
+	}
+
+}
---
+++ garde_manger_back/src/test/java/com/example/garde_manger_back/GardeMangerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garde_manger_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardeMangerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
